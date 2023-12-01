24182b36981cc21c44f74e3e6bf5ba837eff5ec2
Chad Condon <chad@condon.tech>
2023-11-30 22:12:40 -0800

    Organize install files

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -2,27 +2,16 @@ set(INCLUDE_INSTALL_DIR "${CMAKE_INSTALL_INCLUDEDIR}")
 set(LIB_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}")
 set(INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/include")
 
-# Pkg-config file.
-set(prefix "${CMAKE_INSTALL_PREFIX}")
-set(exec_prefix "\${prefix}")
-set(libdir "\${exec_prefix}/${LIB_INSTALL_DIR}")
-set(includedir "\${prefix}/${INCLUDE_INSTALL_DIR}")
-set(PACKAGE_VERSION "${PROJECT_VERSION}")
-
-configure_file(cpputest.pc.in cpputest.pc @ONLY)
-install(
-    FILES "${CMAKE_CURRENT_BINARY_DIR}/cpputest.pc"
-    DESTINATION ${LIB_INSTALL_DIR}/pkgconfig
-)
-
 include(CMakePackageConfigHelpers)
 
 set(CPPUTEST_CONFIG_DEST "${LIB_INSTALL_DIR}/CppUTest/cmake")
 
-configure_package_config_file(CppUTestConfig.cmake.install.in
+configure_package_config_file(
+    ${CMAKE_CURRENT_LIST_DIR}/CppUTestConfig.cmake.install.in
     ${CMAKE_CURRENT_BINARY_DIR}/install/CppUTestConfig.cmake
     INSTALL_DESTINATION "${CPPUTEST_CONFIG_DEST}"
-    PATH_VARS INCLUDE_INSTALL_DIR LIB_INSTALL_DIR)
+    PATH_VARS INCLUDE_INSTALL_DIR LIB_INSTALL_DIR
+)
 write_basic_package_version_file(
     ${CMAKE_CURRENT_BINARY_DIR}/install/CppUTestConfigVersion.cmake
     COMPATIBILITY SameMajorVersion
@@ -39,17 +28,11 @@ install(
     DESTINATION "${CPPUTEST_CONFIG_DEST}"
 )
 install(
-    FILES ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Scripts/CppUTestBuildTimeDiscoverTests.cmake
-    DESTINATION "${CPPUTEST_CONFIG_DEST}/Scripts"
+    DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/Modules"
+    DESTINATION "${CPPUTEST_CONFIG_DEST}"
 )
-install(
-    FILES
-        ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/CppUTestBuildTimeDiscoverTests.cmake
-        ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/CppUTest.cmake
-        ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/_CppUTestDiscovery.cmake
-    DESTINATION "${CPPUTEST_CONFIG_DEST}/Modules"
-)
-configure_package_config_file(CppUTestConfig.cmake.build.in
+configure_package_config_file(
+    ${CMAKE_CURRENT_LIST_DIR}/CppUTestConfig.cmake.build.in
     ${CMAKE_CURRENT_BINARY_DIR}/CppUTestConfig.cmake
     INSTALL_DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
     PATH_VARS INCLUDE_DIR CMAKE_CURRENT_BINARY_DIR
